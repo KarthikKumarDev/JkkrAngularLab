63e6365d7e4c20bc3a9dba356eb5ca15692bd5ae
karthikkumar1996 <jkkr.1996@gmail.com>
2018-11-29 21:13:43 +0530

    Fix: Prod Environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyARqkPvFpXVLvGt7kgzwvOSD9LlZjujFjc",
+    authDomain: "jkkr-lab.firebaseapp.com",
+    databaseURL: "https://jkkr-lab.firebaseio.com",
+    projectId: "jkkr-lab",
+    storageBucket: "jkkr-lab.appspot.com",
+    messagingSenderId: "911166515019"
+  }
 };
